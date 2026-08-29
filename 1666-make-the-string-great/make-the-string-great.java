512c2a49c5fb5b0f1c51ba1620d1d16a63c42b5b
dibyendu2004 <144875644+dibyendu2004@users.noreply.github.com>
2026-08-29 23:12:31 +0530

    Time: 1 ms (99.95%) | Memory: 43.2 MB (91.48%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,19 +1,34 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
+
+        // for(char c:s.toCharArray()){
+        //     if(!st.isEmpty() && (st.peek()-c == 32  || c-st.peek() == 32)){
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(c);
+        //     }
+        // }
+        // StringBuilder ans = new StringBuilder();
+        // while(!st.isEmpty()){
+        //     ans.insert(0,st.pop());
+        // }
+        // return ans.toString();
+
+
+        StringBuilder st = new StringBuilder();
 
         for(char c:s.toCharArray()){
-            if(!st.isEmpty() && (st.peek()-c == 32  || c-st.peek() == 32)){
-                st.pop();
+            int len = st.length();
+
+            if(len>0 && (st.charAt(len-1)-c == 32  || c-st.charAt(len-1) == 32)){
+                st.deleteCharAt(len-1);
             }
             else{
-                st.push(c);
+                st.append(c);
             }
         }
-        StringBuilder ans = new StringBuilder();
-        while(!st.isEmpty()){
-            ans.insert(0,st.pop());
-        }
-        return ans.toString();
+        return st.toString();
     }
 }
